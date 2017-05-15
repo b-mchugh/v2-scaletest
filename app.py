b547d16d37be807531d345d9df0127dabcd3fb40
Brendan Mchugh <bmchugh@redhat.com>
2017-05-15 16:33:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ class Hello(BaseHTTPRequestHandler):
 		self.end_headers()
 		message = [u'Client: ' + self.address_string() + u'<br>' + \
                u'Server: ' + IP + u' aka ' + hostname + u'<br>' + \
-               u'Date: ' + self.date_time_string() + u'<br>' ]
+               u'Date: ' + self.date_time_string() + u'<br>' + \
+               u'Pid: ' + str(os.getpid()) + u'<br>' ]
 		for i in message:
 			print(i)
 			self.wfile.write(str(i).encode("utf8"))
